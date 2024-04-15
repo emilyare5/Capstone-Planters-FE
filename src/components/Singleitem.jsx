26695d732da136114a8ec177549d1ebe96d6e8ad
emilyare5 <emilyarellano1819@gmail>
2024-04-15 18:33:52 -0400

    fix: token based what user can see on single page

--- src/components/Singleitem.jsx
+++ src/components/Singleitem.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { AddCartItem, getSingleInventory } from '../API';
 
@@ -11,24 +11,26 @@ export default function Singleitem({ SetNewItemtoCart, token }) {
     let { itemId } = useParams();
 
     const [quantity, setQuantity] = useState(1);
-    
+
     const [showAlert, setShowAlert] = useState(false);
-   
-    
+
+
+
+
     useEffect(() => {
-        
+
         async function single() {
-            
+
             const data = await getSingleInventory(itemId)
             setSingleData(data);
         }
-        
+
         single();
     }, []);
-    
-    
+
+
     async function addToCart(id, token, quantity) {
-        
+
         try {
             const Add = await AddCartItem(id, token, quantity);
             SetNewItemtoCart(Add);
@@ -37,50 +39,54 @@ export default function Singleitem({ SetNewItemtoCart, token }) {
             console.error(error);
         };
     };
-    
+
     const handleQuantityChange = (event) => {
         setQuantity(event.target.value);
     };
 
     useEffect(() => {
 
-        if(showAlert){
+        if (showAlert) {
 
-           setTimeout(() => {
+            setTimeout(() => {
                 setShowAlert(false);
             }, 3000)
         }
 
     }, [showAlert]);
 
-    
-    
+
+
     return (
         <div>
             {singleData ? (
                 <div>
-                    
+
                     <h2>{singleData.name}</h2>
                     {/* <img src={singleData.imageUrl}/> */}
                     <p>Description: {singleData.description}</p>
                     <p>Price: ${(singleData.price / 100).toFixed(2)}</p>
-                    
-                    <label>
-                        Quantity:
-                        <input
-                            type="number"
-                            value={quantity}
-                            onChange={handleQuantityChange}
-                            min="1"
-                        />
-                    </label>
+
+                    {token &&
+                        <label>
+                            Quantity:
+                            <input
+                                type="number"
+                                value={quantity}
+                                onChange={handleQuantityChange}
+                                min="1"
+                            />
+                        </label>
+                    }
 
                     <div>
                         {quantity === 1 ? showAlert && <div> <p>Added {quantity} {singleData.name} To Cart!</p></div> :
-                        showAlert && <div> <p>Added {quantity} {singleData.name}s To Cart!</p></div> }
+                            showAlert && <div> <p>Added {quantity} {singleData.name}s To Cart!</p></div>}
                     </div>
 
-                    <button onClick={() => addToCart(singleData.id, token, quantity)} >Add To Cart</button>
+                    {token &&
+                        <button onClick={() => addToCart(singleData.id, token, quantity)} >Add To Cart</button>
+                    }
                 </div>
             ) : (
                 <p>Loading...</p>
