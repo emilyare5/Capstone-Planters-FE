e2b80b7fd272ddd985e0ba329a2dc7dc86ab380e
AmiBray <gotstoread@gmail.com>
2024-04-16 20:17:24 -0400

    Feat add prices

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -33,6 +33,7 @@ export default function Inventory() {
                 </div>
                 <div>
                     <img src={imgAddr+item.imgurl} alt={item.name} style={{ maxWidth: '100px', maxHeight: '100px' }} />
+                    <p>Price: ${(item.price / 100).toFixed(2)}</p>
                  </div>
                  
             </div>
