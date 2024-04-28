33ceb01701be57994d045aabf9ad185f9ec1ad79
emilyare5 <emilyarellano1819@gmail>
2024-04-28 18:28:44 -0400

    clean: checkout and cart jsx

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -2,20 +2,20 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
-import OrderConfirmation from './OrderConfirmation';
 import { useNavigate } from 'react-router';
-import Cart from './Cart';
 import { CheckoutCart } from '../API';
 
 function CheckoutForm() {
-    const navigate = useNavigate();
 
-    const handleOnClick = (event) => {
-      event.preventDefault()
-      CheckoutCart();
-      navigate('/completedorder');
-    };
-    
+  const navigate = useNavigate();
+
+  const handleOnClick = (event) => {
+    event.preventDefault()
+    CheckoutCart();
+    navigate('/completedorder');
+
+  };
+
   return (
     <Form className='checkform'>
       <Row className="mb-3">
@@ -95,7 +95,7 @@ function CheckoutForm() {
             <option>West Virginia</option>
             <option>Wisconsin</option>
             <option>Wyoming</option>
-        
+
           </Form.Select>
         </Form.Group>
 
@@ -116,85 +116,85 @@ function CheckoutForm() {
         </Form.Group>
       </Row>
       <Row>
-      <Form.Group className="mb-3" controlId="formGridAddress1">
-        <Form.Label>Billing Address</Form.Label>
-        <Form.Control placeholder="1234 Main St" />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formGridAddress2">
-        <Form.Label>Address Line 2</Form.Label>
-        <Form.Control placeholder="Apartment, studio, or floor" />
-      </Form.Group>
-
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridCity">
-          <Form.Label>City</Form.Label>
-          <Form.Control />
+        <Form.Group className="mb-3" controlId="formGridAddress1">
+          <Form.Label>Billing Address</Form.Label>
+          <Form.Control placeholder="1234 Main St" />
         </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridState">
-          <Form.Label>State</Form.Label>
-          <Form.Select defaultValue="Choose...">
-            <option>Choose...</option>
-            <option>Alabama</option>
-            <option>Alaska</option>
-            <option>Arizona</option>
-            <option>Arkansas</option>
-            <option>California</option>
-            <option>Colorado</option>
-            <option>Connecticut</option>
-            <option>Delaware</option>
-            <option>Florida</option>
-            <option>Georgia</option>
-            <option>Hawaii</option>
-            <option>Idaho</option>
-            <option>Illinois</option>
-            <option>Indiana</option>
-            <option>Iowa</option>
-            <option>Kansas</option>
-            <option>Kentucky</option>
-            <option>Louisiana</option>
-            <option>Maine</option>
-            <option>Maryland</option>
-            <option>Massachusetts</option>
-            <option>Michigan</option>
-            <option>Minnesota</option>
-            <option>Mississippi</option>
-            <option>Missouri</option>
-            <option>Montana</option>
-            <option>Nebraska</option>
-            <option>Nevada</option>
-            <option>New Hampshire</option>
-            <option>New Jersey</option>
-            <option>New Mexico</option>
-            <option>New York</option>
-            <option>North Carolina</option>
-            <option>North Dakota</option>
-            <option>Ohio</option>
-            <option>Oklahoma</option>
-            <option>Oregon</option>
-            <option>Pennsylvania</option>
-            <option>Rhode Island</option>
-            <option>South Carolina</option>
-            <option>South Dakota</option>
-            <option>Tennessee</option>
-            <option>Texas</option>
-            <option>Utah</option>
-            <option>Vermont</option>
-            <option>Virginia</option>
-            <option>Washington</option>
-            <option>West Virginia</option>
-            <option>Wisconsin</option>
-            <option>Wyoming</option>
-        
-          </Form.Select>
+        <Form.Group className="mb-3" controlId="formGridAddress2">
+          <Form.Label>Address Line 2</Form.Label>
+          <Form.Control placeholder="Apartment, studio, or floor" />
         </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridZip">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control />
-        </Form.Group>
-      </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridCity">
+            <Form.Label>City</Form.Label>
+            <Form.Control />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Label>State</Form.Label>
+            <Form.Select defaultValue="Choose...">
+              <option>Choose...</option>
+              <option>Alabama</option>
+              <option>Alaska</option>
+              <option>Arizona</option>
+              <option>Arkansas</option>
+              <option>California</option>
+              <option>Colorado</option>
+              <option>Connecticut</option>
+              <option>Delaware</option>
+              <option>Florida</option>
+              <option>Georgia</option>
+              <option>Hawaii</option>
+              <option>Idaho</option>
+              <option>Illinois</option>
+              <option>Indiana</option>
+              <option>Iowa</option>
+              <option>Kansas</option>
+              <option>Kentucky</option>
+              <option>Louisiana</option>
+              <option>Maine</option>
+              <option>Maryland</option>
+              <option>Massachusetts</option>
+              <option>Michigan</option>
+              <option>Minnesota</option>
+              <option>Mississippi</option>
+              <option>Missouri</option>
+              <option>Montana</option>
+              <option>Nebraska</option>
+              <option>Nevada</option>
+              <option>New Hampshire</option>
+              <option>New Jersey</option>
+              <option>New Mexico</option>
+              <option>New York</option>
+              <option>North Carolina</option>
+              <option>North Dakota</option>
+              <option>Ohio</option>
+              <option>Oklahoma</option>
+              <option>Oregon</option>
+              <option>Pennsylvania</option>
+              <option>Rhode Island</option>
+              <option>South Carolina</option>
+              <option>South Dakota</option>
+              <option>Tennessee</option>
+              <option>Texas</option>
+              <option>Utah</option>
+              <option>Vermont</option>
+              <option>Virginia</option>
+              <option>Washington</option>
+              <option>West Virginia</option>
+              <option>Wisconsin</option>
+              <option>Wyoming</option>
+
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridZip">
+            <Form.Label>Zip</Form.Label>
+            <Form.Control />
+          </Form.Group>
+        </Row>
       </Row>
 
       <Form.Group className="mb-3" controlId="formGridCardNum">
@@ -205,12 +205,12 @@ function CheckoutForm() {
       <Row className="mb-3">
         <Form.Group as={Col} controlId="formGridSecurityCode">
           <Form.Label>Security Code</Form.Label>
-          <Form.Control placeholder="123"/>
+          <Form.Control placeholder="123" />
         </Form.Group>
 
         <Form.Group as={Col} controlId="formGridExpiration">
           <Form.Label>Expiration Date</Form.Label>
-          <Form.Control placeholder="MM/YY"/>
+          <Form.Control placeholder="MM/YY" />
         </Form.Group>
 
         <Form.Group as={Col} controlId="formGridZip">
@@ -225,6 +225,6 @@ function CheckoutForm() {
       </Button>
     </Form>
   );
-}
+};
 
 export default CheckoutForm;
