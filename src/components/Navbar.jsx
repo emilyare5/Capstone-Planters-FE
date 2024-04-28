77ff777ce1d00d2cc8fdd6918cbb135f56899f15
emilyare5 <emilyarellano1819@gmail>
2024-04-28 18:46:58 -0400

    clewn: NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,10 @@ import Button from 'react-bootstrap/Button';
 import Cookies from 'universal-cookie';
 import { logoutCustomer, getUserAccess } from '../API';
 
-export default function Navibar({isLoggedIn}) {
+export default function Navibar({ isLoggedIn }) {
+
     const cookies = new Cookies();
+
     const [userAccess, setUserAccess] = useState({
         custId: "",
         username: "",
@@ -18,65 +20,85 @@ export default function Navibar({isLoggedIn}) {
     });
 
     useEffect(() => {
+
         async function getUserAuth() {
+
             const user = await getUserAccess()
+
             setUserAccess({
                 custId: user.custId,
                 username: user.username,
                 role: user.role,
                 isAdmin: user.isAdmin
             })
-        }
+
+        };
+
         getUserAuth()
-    }, [isLoggedIn])
+
+    }, [isLoggedIn]);
 
     async function logOut() {
-        logoutCustomer()
-        cookies.remove("isLoggedIn", false)
+
+        logoutCustomer();
+        cookies.remove("isLoggedIn", false);
         window.location.reload();
-    }
+
+    };
 
 
     {
         userAccess.username ? <button onClick={logOut}>Log Out</button>
             : <button><Link to="/login">Login</Link></button>
-    }
+    };
+
     {
         userAccess.username ? null
             : <button><Link to="/register">Register New User</Link></button>
-    }
+    };
 
     function LoggedInNavs() {
+
         return (
             <>
-                    <NavDropdown title={userAccess.username} id="basic-nav-dropdown">
+                <NavDropdown title={userAccess.username} id="basic-nav-dropdown">
+
                     <Nav.Item>
                         <Button onClick={logOut} variant="secondary" size="sm">Log Out</Button>
                     </Nav.Item>
+
                     <NavDropdown.Item href="/mycart">My Cart</NavDropdown.Item>
+
                     <NavDropdown.Item href="#cart">My Account</NavDropdown.Item>
+
                     <NavDropdown.Divider />
+
                     {
                         userAccess.isAdmin ? <AdminNavs />
                             : null
                     }
+
                 </NavDropdown>
             </>
-        )
-    }
+        );
+    };
 
     function LoggedOutNavs() {
+
         return (
             <>
                 <Nav.Item>
                     <Nav.Link href="/login">Login</Nav.Link>
                 </Nav.Item>
+
+
                 <Nav.Item>
                     <Nav.Link href="/register">Register Now</Nav.Link>
                 </Nav.Item>
+
             </>
-        )
-    }
+        );
+    };
 
     function AdminNavs() {
         return (
@@ -86,29 +108,34 @@ export default function Navibar({isLoggedIn}) {
                 <NavDropdown.Item href="/admin/inventory">ADMIN - All Inventory</NavDropdown.Item>
                 <NavDropdown.Divider />
             </>
-        )
-    }
+        );
+    };
 
     return (
         <Navbar id="navbar" expand="lg" className="bg-body-tertiary">
+
             <Container>
                 <Navbar.Brand href="/">Planters Store</Navbar.Brand>
+
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
+
                         <Nav.Item>
                             <Nav.Link href="/">Shop Now</Nav.Link>
                         </Nav.Item>
+
                         {
                             userAccess.username ? <LoggedInNavs />
                                 : <LoggedOutNavs />
                         }
 
-
                     </Nav>
                 </Navbar.Collapse>
             </Container>
+
         </Navbar>
 
-    )
-}
+    );
+};
